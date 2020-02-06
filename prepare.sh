1f8bfd60351a27361fb7e33dd8de797d31651a59
subhead <subhead@gmail.com>
2020-02-06 23:35:09 +0100

    some webcam scripts

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-apt install build-essential python3.6 python3.6-venv python3-pip python3-psycopg2 libgpiod2
+apt install build-essential python3.6 python3.6-venv python3-pip python3-psycopg2 libgpiod2 uvcdynctrl
+
+# https://raspberrypi.stackexchange.com/questions/43118/turning-off-the-blue-status-led-on-the-logitech-c920-usb-camera
+uvcdynctrl -i /usr/share/uvcdynctrl/data/046d/logitech.xml
+
+# LED off
+uvcdynctrl -s 'LED1 Mode' 0
+# LED on
+#uvcdynctrl -s 'LED1 Mode' 1
+# LED blinking
+#uvcdynctrl -s 'LED1 Mode' 2
+# LED auto mode
+#uvcdynctrl -s 'LED1 Mode' 3
