ce960c74fadb240e5d891bd1c78db336b201293b
subhead <subhead@gmail.com>
2020-03-02 15:27:18 +0100

    removed debug print statements

--- planter/dashboard/__init__.py
+++ planter/dashboard/__init__.py
@@ -5,11 +5,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-print(template_dir)
 template_dir = os.path.join(template_dir, 'planter')
-print(template_dir)
 template_dir = os.path.join(template_dir, 'dashboard')
-print(template_dir)
 template_dir = os.path.join(template_dir, 'static')
 template_dir = os.path.join(template_dir, 'templates')
 
